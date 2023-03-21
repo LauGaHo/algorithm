7b0d7bc3a80728fff18a15bb7d0c05313982a593
刘嘉豪 <cattou147@icloud.com>
2023-03-21 13:15:52 +0800

    feat: leetcode-123-滚动数组方法

--- 动态规划/leetcode-123-买卖股票的最佳时机III.js
+++ 动态规划/leetcode-123-买卖股票的最佳时机III.js
@@ -1,4 +1,5 @@
 /**
+ * 二维数组方法
  * @param {number[]} prices
  * @return {number}
  */
@@ -47,4 +48,49 @@ var maxProfit_1 = function (prices) {
   return dp[prices.length - 1][4];
 };
 
+// 二维数组方法
 maxProfit_1([3, 3, 5, 0, 0, 3, 1, 4]);
+
+/**
+ * 滚动数组方法
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit_2 = function (prices) {
+  // 创建 dp 数组，dp 数组的含义如下所示：
+  // dp[0]: 不操作
+  // dp[1]: 第一次持有
+  // dp[2]: 第一次不持有
+  // dp[3]: 第二次持有
+  // dp[4]: 第二次不持有
+  const dp = [0, -prices[0], 0, -prices[0], 0];
+
+  // 循环
+  for (let i = 1; i < prices.length; i++) {
+    // 第二次不持有最大现金和
+    dp[4] = Math.max(
+      dp[4], // 前一天第二次不持有
+      dp[3] + prices[i] // 前一天第二次持有，今天卖出
+    );
+    // 第二次持有最大现金和
+    dp[3] = Math.max(
+      dp[3], // 前一天第二次持有
+      dp[2] - prices[i] // 前一天第一次不持有，今天买入
+    );
+    // 第一次不持有最大现金和
+    dp[2] = Math.max(
+      dp[2], // 前一天第一次不持有
+      dp[1] + prices[i] // 前一天第一次持有，今天卖出
+    );
+    // 第一次持有最大现金和
+    dp[1] = Math.max(
+      dp[1], // 前一天第一次持有
+      dp[0] - prices[i] // 前一天不操作，今天买入
+    );
+  }
+
+  return dp[4];
+};
+
+// 滚动数组方法
+maxProfit_2([3, 3, 5, 0, 0, 3, 1, 4]);
